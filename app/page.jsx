14294f3996ae15950e9fd95454985fd5f5b69296
MatheusScaranello <matheusscaranello.564@gmail.com>
2023-11-20 17:09:04 -0300

    Add API call to retrieve list of players

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,16 @@
 "use client"
+import { useState } from 'react'
+import Axios from 'axios'
+
+  const api = Axios.create({
+    baseURL: 'http://localhost:4000'
+  });
 
 export default function Home() {
+
+  api.get('/jogadores').then(response => {
+    console.log(response);
+    })
 
   return (
     <div>
